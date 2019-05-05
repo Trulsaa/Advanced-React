f87ab1ca213a2a05e20a631e3053b2e2f17e60bc
Truls Aagaard <truls.aa@gmail.com>
2019-05-05 21:26:46 +0200

    Create PleaseSignIn wrapper

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -6,9 +6,21 @@ const { transport, makeANiceEmail } = require("../mail");
 
 const mutations = {
   async createItem(parent, args, ctx, info) {
-    // TODO: Check if they are logged in
+    if (!ctx.request.userId)
+      throw new Error("You must be logged in to do that!");
 
-    const item = await ctx.db.mutation.createItem({ data: { ...args } }, info);
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          // This is how to create a relationship between the Item and the User
+          user: {
+            connect: { id: ctx.request.userId }
+          },
+          ...args
+        }
+      },
+      info
+    );
     return item;
   },
 
